5151f6882b47990b8ff62f86abb83dc563833ccc
Alex Fábián <alex@crowdworks.it>
2022-03-13 15:43:17 +0100

    project setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Calendar Booking backend
+
+### How to run
+
+1.  Install dependencies with `npm install`
+1.  Start dev server with `npm start` **or** Run project with `npm run start-server`
---
+++ server.js
@@ -0,0 +1,21 @@
+const { graphql, buildSchema } = require("graphql");
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const rootValue = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+
+graphql({
+  schema,
+  source: "{ hello }",
+  rootValue,
+}).then((response) => {
+  console.log(response);
+});
