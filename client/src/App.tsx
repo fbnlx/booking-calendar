1349e7fd8340c7bcc437607b09fcb0d6c630b28f
Alex Fábián <alex@crowdworks.it>
2022-03-15 14:46:31 +0100

    component styling, convert iso date

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,28 +23,40 @@ function App() {
         <form onSubmit={handleSubmit}>
           <label htmlFor="bookingDuration">
             Please provide a booking duration in minutes
+          </label>
+          <div className="input__container">
             <input
+              id="bookingDuration"
               name="bookingDuration"
               type="number"
               value={duration}
               onChange={(e) => setDuration(e.target.value)}
               min={1}
             />
-          </label>
-          <button type="submit" disabled={loading}>
-            {loading ? "Fetching..." : "Check availability"}
-          </button>
+            <button type="submit" disabled={loading}>
+              {loading ? "Fetching..." : "Check availability"}
+            </button>
+          </div>
         </form>
       </article>
-      {data &&
-        data.booking.map((booking, idx) => (
-          <TimeSlot
-            key={`booking-${idx}`}
-            photographer={booking.photographer.name}
-            startTime={booking.timeSlot.starts}
-            endTime={booking.timeSlot.ends}
-          />
-        ))}
+      {data && data.booking.length > 0 && (
+        <div>
+          <span className="results_text">Available time slots:</span>
+          {data.booking.map((booking, idx) => (
+            <TimeSlot
+              key={`booking-${idx}`}
+              photographer={booking.photographer.name}
+              startTime={booking.timeSlot.starts}
+              endTime={booking.timeSlot.ends}
+            />
+          ))}
+        </div>
+      )}
+      {data && data.booking.length === 0 && (
+        <div style={{ margin: "4rem", fontWeight: "bold", color: "#c95d5d" }}>
+          No available time slots for this duration
+        </div>
+      )}
       {error && (
         <div>
           <span>Failed to fetch</span>
--- client/src/components/TimeSlot.tsx
+++ client/src/components/TimeSlot.tsx
@@ -23,12 +23,24 @@ export const GET_AVAILABLE_TIME_SLOTS = gql`
   }
 `;
 
+const getDateTimeFormat = (dateISO: string) => {
+  const date = new Date(dateISO);
+  const time = new Date(dateISO).toLocaleTimeString("en", {
+    timeStyle: "short",
+    hour12: false,
+    timeZone: "UTC",
+  });
+  return `${date.getDate()}.${
+    date.getMonth() + 1
+  }.${date.getFullYear()} ${time}`;
+};
+
 export default function TimeSlot({ photographer, startTime, endTime }: Props) {
   return (
-    <article>
+    <article className="timeslot__container">
       <h1>{photographer}</h1>
       <time>
-        {startTime} - {endTime}
+        {getDateTimeFormat(startTime)} - {getDateTimeFormat(endTime)}
       </time>
     </article>
   );
